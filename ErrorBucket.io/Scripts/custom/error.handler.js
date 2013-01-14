19d7a0e154a6f1a3f644cb7238dc6bd80bb7c487
Danny Lane <dannylane@gmail.com>
2013-01-14 11:07:48 +0000

    Add some exception handling to the error handler

--- ErrorBucket.io/Scripts/custom/error.handler.js
+++ ErrorBucket.io/Scripts/custom/error.handler.js
@@ -4,12 +4,14 @@ var count = 0;
 var src = 'http://dannytesting3.azurewebsites.net/log?error=';
 
 window.onerror = function (errorMsg, url, lineNumber) {
+    try{
     var messageToLog = errorMsg + '&url=' + url + '&lineNumber=' + lineNumber + '&localTime=' + Date.now();
     cachedErrrors.push(messageToLog);
 
     var bodyEle = document.getElementsByTagName('body')[0];
     if (bodyEle) {
         while (cachedErrrors.length > 0) {
+            alert('yep: ' + errorMsg);
             var imgElement = document.createElement('img');
             imgElement.setAttribute('id', 'error-bucket-image-' + count);
             imgElement.setAttribute('src', src + cachedErrrors.pop());
@@ -21,5 +23,8 @@ window.onerror = function (errorMsg, url, lineNumber) {
     if (currentHandler) {
         return currentHandler(errorMsg, url, lineNumber);
     }
+    } catch(ex){
+        alert('nope: ' + ex);
+    }    
     return false;
 }
